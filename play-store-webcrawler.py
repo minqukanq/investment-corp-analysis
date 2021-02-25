e69ee7711631d37da0c328985b1eba1cd8c02e73
Seongbin Lim <lim.andrew1@gmail.com>
2021-02-25 17:55:22 +0900

    update code

--- play-store-webcrawler.py
+++ play-store-webcrawler.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import time
 import pandas as pd
+from bs4 import BeautifulSoup
+import re
 
 def crawl_google_playstore(url, driverPath):
     # Open url with fullscreen
@@ -23,7 +25,7 @@ def crawl_google_playstore(url, driverPath):
         
         else:
             break
-        
+
     # Spread long reviews
     driver.execute_script("window.scrollTo(document.body.scrollHeight, 0)")
     spread_review = driver.find_elements_by_xpath("//button[@jsaction='click:TiglPc']")
@@ -35,29 +37,66 @@ def crawl_google_playstore(url, driverPath):
             spread_review[i].click()
             print(str(i)+"th more button is clicked and wait 2 secs...")
             time.sleep(2)
-            
-    # Separate short and long reviews
-    reviews = driver.find_elements_by_xpath("//span[contains(@jsname, 'bN97Pc')]")
-    long_reviews = driver.find_elements_by_xpath("//span[@jsname='fbQN7e']")
-        
-    # Merge all reviews
-    merged_review = [t.text if t.text!='' else long_reviews[i].text for i, t in enumerate(reviews)]
 
-    # Scrape dates, likes, and rating scores
-    dates = driver.find_elements_by_xpath("//span[@class='p2TkOb']")
-    likes = driver.find_elements_by_xpath("//div[@aria-label='이 리뷰가 유용하다는 평가를 받은 횟수입니다.']")
-    stars = driver.find_elements_by_xpath("//span[@class='nt2C1d']/div[@class='pf5lIe']/div[@role='img']")
+
+# ---------------------------------------
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'lxml')
+    div_reviews = soup.find_all("div", {"class":"d15Mdf bAhLNe"})
+    reviews = []
+
+    for div in div_reviews:
+        # grade = len(div.find_all('div',{'class' : 'nt2C1d'}))
+        grade = div.find('div',{"class":"pf5lIe"}).find_next()['aria-label']
+        print(grade)
+        
+        date_text = div.find('span',{"class":"p2TkOb"}).get_text()
+        t = re.findall(r"\d*\.\d+|\d+",date_text) 
+        date = '{0}-{1}-{2}'.format(t[0],t[1],t[2])
+        print(date)
+        good = div.find('div',{"class":"YCMBp GVFJbb"}).get_text()
+        print(good)
+        short_content = div.find('span',{'jsname':'bN97Pc'}).get_text()
+        long_content = div.find('span',{'jsname':'fbQN7e'}).get_text()
+        content = short_content if long_content=='' else long_content
+        content.encode("utf-8")
+        print(content)
+        reviews.append((date,grade,good,content))
+
+
     
-    # Make a dataframe
-    res_dict = []
-    for i in range(len(merged_review)):
-        res_dict.append({
-            'DATE' : dates[i].text, 
-            'STAR' : stars[i].get_attribute('aria-label'), 
-            'LIKE' : likes[i].text, 
-            'REVIEW' : merged_review[i]
-        })
+    print(len(reviews))
     
-    res_df = pd.DataFrame(res_dict)
+    return reviews
+    # # Separate short and long reviews
+    # reviews = driver.find_elements_by_xpath("//span[contains(@jsname, 'bN97Pc')]")
+    # long_reviews = driver.find_elements_by_xpath("//span[@jsname='fbQN7e']")
+        
+    # # Merge all reviews
+    # merged_review = [t.text if t.text!='' else long_reviews[i].text for i, t in enumerate(reviews)]
+
+    # # Scrape dates, likes, and rating scores
+    # dates = driver.find_elements_by_xpath("//span[@class='p2TkOb']")
+    # likes = driver.find_elements_by_xpath("//div[@aria-label='이 리뷰가 유용하다는 평가를 받은 횟수입니다.']")
+    # stars = driver.find_elements_by_xpath("//span[@class='nt2C1d']/div[@class='pf5lIe']/div[@role='img']")
     
-    return res_df
+    # # Make a dataframe
+    # res_dict = []
+    # for i in range(len(merged_review)):
+    #     res_dict.append({
+    #         'DATE' : dates[i].text, 
+    #         'STAR' : stars[i].get_attribute('aria-label'), 
+    #         'LIKE' : likes[i].text, 
+    #         'REVIEW' : merged_review[i]
+    #     })
+    
+    # res_df = pd.DataFrame(res_dict)
+    
+    # return res_df
+
+url = "https://play.google.com/store/apps/details?id=com.linkzen.app&showAllReviews=true"
+driverPath = "chromedriver.exe"
+reviews = crawl_google_playstore(url, driverPath)
+# display(res_df)
+
+# res_df.to_csv('kiwoom.csv')
